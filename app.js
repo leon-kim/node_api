207d31be8a0b71fbe8659f0eea3696e98986a3fa
Leon Kim <leon.kim@findly.com>
2014-12-11 15:58:57 +1300

    all crud

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ router.use(function(req, res, next){
 });
 
 router.route('/users')
+  .get(function (req, res){
+    User.find(function (err, users){
+      if (err) res.send(err);
+
+      res.json(users);
+    });
+  })
   .post(function (req, res){
     var user = new User();
     user.name = req.body.name;
@@ -52,8 +59,18 @@ router.route('/users/:user_id')
         res.json({message: 'User updated!'});
       })
     })
+  })
+  .delete(function (req, res){
+    User.remove({
+      _id: req.params.user_id
+    },function(err, user){
+      if (err) res.send(err);
+
+      res.json({message: 'deleted'}); 
+    });
   });
 
+// all of routes will be prefixed with /api
 app.use('/api', router);
 
 
